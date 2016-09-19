7404724821aab6c1011e8cc72dc2b8f8ce907346
biao <biao@admin.com>
2016-09-19 09:26:26 +0800

    修改一些bug

--- data/config.php
+++ data/config.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 // database host
 $db_host   = "120.25.84.174:3306";
@@ -36,43 +35,3 @@ define('OLD_AUTH_KEY', '');
 
 define('API_TIME', '2016-09-18 07:56:48');
 ?>
-=======
-<?php
-// database host
-$db_host   = "120.25.84.174:3306";
-
-// database name
-$db_name   = "jingdong";
-
-// database username
-$db_user   = "root";
-
-// database password
-$db_pass   = "admin";
-
-// table prefix
-$prefix    = "ecs_";
-
-$timezone    = "UTC";
-
-$cookie_path    = "/";
-
-$cookie_domain    = "";
-
-$session = "1440";
-
-define('EC_CHARSET','utf-8');
-
-if(!defined('ADMIN_PATH'))
-{
-define('ADMIN_PATH','admin');
-}
-
-define('AUTH_KEY', 'this is a key');
-
-define('OLD_AUTH_KEY', '');
-
-define('API_TIME', '2016-09-19 01:04:13');
-
-?>
->>>>>>> e66b49522dd4ce4270448d94c0b587cc2b4c9592
