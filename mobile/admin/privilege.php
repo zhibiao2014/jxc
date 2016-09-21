be6cfa38be822440b8aae7279f5af2dc42f0b73f
biao <biao@admin.com>
2016-09-21 09:52:36 +0800

    修改手机后台退出登录跳转地址

--- mobile/admin/privilege.php
+++ mobile/admin/privilege.php
@@ -48,6 +48,7 @@ if ($_REQUEST['act'] == 'logout')
 /*------------------------------------------------------ */
 if ($_REQUEST['act'] == 'login')
 {
+	ecs_header("Location: /admin\n");exit;
     header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
     header("Cache-Control: no-cache, must-revalidate");
     header("Pragma: no-cache");
